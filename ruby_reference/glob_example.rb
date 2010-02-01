857d6f9e5750e43b621b7eda49c4ad659747f494
Elricstorm <jdezenzio@gmail.com>
2010-02-01 17:25:39 -0500

    Documentation updated.

--- ruby_reference/glob_example.rb
+++ ruby_reference/glob_example.rb
@@ -2,6 +2,6 @@
 # Globing files is fine for external source files like .dlls or .png etc. but not
 # for ruby files.  You should use sane requires and loads for requiring/loading ruby
 # files.
-Dir[File.join(File.dirname(__FILE__),"ui","*.png")].each { |img_files| some_command img_files }
-Dir[File.expand_path('../../ui/*.png', __FILE__)].each { |img_files| some_command img_files }
+Dir[File.join(File.dirname(__FILE__),"images","*.png")].each { |img_files| some_command img_files }
+Dir[File.expand_path('images/*.png', __FILE__)].each { |img_files| some_command img_files }
 
